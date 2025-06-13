02dd3355d66347ceb042f587124890498a4d91ed
myllmat22 <matiasmyllymaki05@gmail.com>
2025-06-13 19:31:04 +0300

    juttuja tehty

---
+++ project_mirror/scripts/the_boy.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_pressed("right"):
+		velocity.x =  1 * SPEED
+	else: if Input.is_action_pressed("left"):
+		velocity.x = -1 * SPEED
+	else:
+		velocity.x = 0
+	move_and_slide()
