843ff6c8abfd6b86186ca5b038114fae9ebb9e26
myllmat22 <matiasmyllymaki05@gmail.com>
2025-06-13 21:56:28 +0300

    double jumping

--- project_mirror/scripts/mirrored_boy.gd
+++ project_mirror/scripts/mirrored_boy.gd
@@ -4,15 +4,21 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
+var double_jump: bool = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if is_on_floor():
+		double_jump = true;
 
 	# Handle jump.
 	if Input.is_action_just_pressed("uparrow") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("uparrow") && !is_on_floor() && double_jump:
+		velocity.y = JUMP_VELOCITY
+		double_jump = false;
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
--- project_mirror/scripts/the_boy.gd
+++ project_mirror/scripts/the_boy.gd
@@ -4,15 +4,23 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
+var double_jump: bool = true
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if is_on_floor():
+		double_jump = true;
 
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if Input.is_action_just_pressed("up") && is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("up") && !is_on_floor() && double_jump:
+		velocity.y = JUMP_VELOCITY
+		double_jump = false;
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
