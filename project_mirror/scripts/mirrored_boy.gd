8c149c96cbb54a6aa8f17f51b9a7e7e37d94e8a9
myllmat22 <matiasmyllymaki05@gmail.com>
2025-06-14 13:51:23 +0300

    did stuff

--- project_mirror/scripts/mirrored_boy.gd
+++ project_mirror/scripts/mirrored_boy.gd
@@ -5,8 +5,10 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 const DOUBLE_JUMP_VELOCITY = -400.0
 var isplane: bool = false
+var canjump = true
 
 @onready var timer: Timer = $Timer
+@onready var timer_2: Timer = $Timer2
 
 var double_jump: bool = true
 var anim2play: String = "Idle"
@@ -21,14 +23,19 @@ func _physics_process(delta: float) -> void:
 			velocity += get_gravity() * delta
 		if is_on_floor():
 			double_jump = true;
+		if canjump == false and is_on_floor():
+			canjump = true
 	
 		# Handle jump.
-		if Input.is_action_just_pressed("uparrow") && is_on_floor():
-			velocity.y = JUMP_VELOCITY
-			
-		if Input.is_action_just_pressed("uparrow") && !is_on_floor() && double_jump:
+		if (is_on_floor() == false) and canjump and timer_2.is_stopped():
+			timer_2.start()
+		if Input.is_action_just_pressed("uparrow") && !is_on_floor() && double_jump && !canjump:
 			velocity.y = DOUBLE_JUMP_VELOCITY
 			double_jump = false;
+		# Handle jump.
+		if Input.is_action_just_pressed("uparrow") && canjump:
+			velocity.y = JUMP_VELOCITY
+			canjump = false
 	
 		if Input.is_action_pressed("rightarrow"):
 			velocity.x =  1 * SPEED * delta * 60
@@ -52,5 +59,6 @@ func _physics_process(delta: float) -> void:
 	
 func _on_timer_timeout() -> void:
 	isplane = true
-	
-	
+
+func _on_timer_2_timeout() -> void:
+	canjump = false
--- project_mirror/scripts/the_boy.gd
+++ project_mirror/scripts/the_boy.gd
@@ -1,27 +1,37 @@
 extends CharacterBody2D
 
+@onready var timer: Timer = $Timer
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 const DOUBLE_JUMP_VELOCITY = -400.0
 
+var canjump: bool = true
+
 var double_jump: bool = true
 var anim2play: String = "Idle"
 @export var animatedSprite2d: AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if is_on_floor():
 		double_jump = true;
-
-	# Handle jump.
-	if Input.is_action_just_pressed("up") && is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if canjump == false and is_on_floor():
+		canjump = true
 		
-	if Input.is_action_just_pressed("up") && !is_on_floor() && double_jump:
+	if (is_on_floor() == false) and canjump and timer.is_stopped():
+		timer.start()
+	if Input.is_action_just_pressed("up") && !is_on_floor() && double_jump && !canjump:
 		velocity.y = DOUBLE_JUMP_VELOCITY
 		double_jump = false;
+	# Handle jump.
+	if Input.is_action_just_pressed("up") && canjump:
+		velocity.y = JUMP_VELOCITY
+		canjump = false
+		
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -41,4 +51,5 @@ func _physics_process(delta: float) -> void:
 		animatedSprite2d.play(anim2play)
 	animatedSprite2d.play(anim2play)
 	
-	
+func _on_timer_timeout() -> void:
+	canjump = false
