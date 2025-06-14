3cab3536664f120e0c7876dccfd3316b75e420b7
RPGArgon <eeli@dadu.fi>
2025-06-14 19:03:02 +0300

    Made Bullets work

--- project_mirror/Scripts/bullet.gd
+++ project_mirror/Scripts/bullet.gd
@@ -1,11 +1,10 @@
-class_name Bullet extends Node
+class_name Bullet extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var speed = 750
 
+func _physics_process(delta):
+	position -= transform.y * speed * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_Bullet_body_entered(body):
+	queue_free()
--- project_mirror/Scripts/linked_breakable_block.gd
+++ project_mirror/Scripts/linked_breakable_block.gd
@@ -1,4 +1,4 @@
-extends Node2D
+class_name LinkeBreakable extends Node2D
 
 
 @onready var real = $RealSprite
