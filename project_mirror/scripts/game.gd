0f1d21a90d791a2d16794951c41a124b44f066ac
RPGArgon <eeli@dadu.fi>
2025-06-15 14:01:26 +0300

    changes

--- project_mirror/scripts/game.gd
+++ project_mirror/scripts/game.gd
@@ -2,11 +2,12 @@ class_name Game extends Node2D
 
 @onready var ui: UI = $Camera2D/Ui
 var score: float = 0
-
+var pauseMenu = load("res://Scenes/PauseMenu.tscn")
 func addtoscore():
 	score = score + 50
 	
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	AudioServer.set_bus_volume_db(
 		0,
 		linear_to_db(HighScore.volume)
@@ -16,3 +17,8 @@ func _process(delta: float) -> void:
 	score += 2*delta
 	ui.updateScore(score)
 	ui.updateHighScore(score)
+	
+	if Input.is_action_just_pressed("Pause"):
+		get_tree().paused = true
+		var newMenu = pauseMenu.instantiate()
+		add_child(newMenu)
